15d1efe08144992b6c8e8f872dff03df61f0b5ce
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-09 05:58:16 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/09.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
