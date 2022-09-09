e67db09276dc5446b700ab8b8df8e209ad28cf3e
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-09 10:10:31 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,50 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/09.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }) //: BUTTON
+            
+            Spacer()
+            
+            ZStack {
+                Button(action: {}, label: {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                })
+            } //: BUTTON
+        } //: HSTACK
+    }
+}
+
+
+// MARK: - PREVIEW
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
