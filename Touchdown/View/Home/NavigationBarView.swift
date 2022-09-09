feb4eb60c9aa8f2fc27bfb4c7bdfa1ba63fd0886
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-09 11:19:59 +0900

    Swift進捗
    Touchdownアプリの作成

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct NavigationBarView: View {
     // MARK: - PROPERTIES
     
+    @State private var isAnimated: Bool = false
+    
     // MARK: - BODY
     
     var body: some View {
@@ -19,6 +21,17 @@ struct NavigationBarView: View {
                     .font(.title)
                     .foregroundColor(.black)
             }) //: BUTTON
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
             
             Spacer()
             
