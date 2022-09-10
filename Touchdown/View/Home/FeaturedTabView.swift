ed81ef84a183f05c52d0e6b175479d4ce5a48bcf
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-11 04:48:22 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,28 @@
+//
+//  FeaturedTabView.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/11.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+            }
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .background(Color.gray)
+    }
+}
