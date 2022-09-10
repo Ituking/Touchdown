1cbdace38ff2cd2866ee9e95ecc2f1f5079ab4c9
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-11 04:15:58 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/11.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
