a47e05aa8159f6e65360d652fcccc2ee09a4eed4
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-12 16:57:56 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/12.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5, content: {
+            // NAVBAR
+            
+            // HEADER
+            Text(sampleProduct.name)
+            
+            // DETAIL TOP PART
+            // DETAIL BOTTOM PART
+            // RATING + SIZES
+            // DESCRIPTION
+            // QUANTITY + FAVOURITE
+            
+            // ADD TO CART
+            Spacer()
+        }) //: VSTACK
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue
+            ).ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+// MARK: - PREVIEW
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
