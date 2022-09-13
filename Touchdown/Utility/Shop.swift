04aef7d58b4596ee89846a85ccb5a47a700b9393
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-13 13:30:34 +0900

    Swift進捗
    Touchdownアプリの作成

---
+++ Touchdown/Utility/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  Touchdown
+//
+//  Created by 大久保樹 on 2022/09/13.
+//
+
+import Foundation
+
+class Shop: ObservableObject {
+    var showingProduct: Bool = false
+    var selectedProduct: Product? = nil
+}
